0c032ec269c36e8f8cdce6dad76e5d349dc515e9
Ry Horsey <horsey.henry@gmail.com>
2016-01-06 14:03:17 -0700

    Initial local file creation and todos

---
+++ server/lib/analysis/local_runner.rb
@@ -0,0 +1,112 @@
+class Analysis::BatchRun
+  include Analysis::Core
+
+  def initialize(analysis_id, analysis_job_id, options = {})
+    defaults = {
+      skip_init: false,
+      data_points: [],
+      run_data_point_filename: 'run_openstudio.rb',
+      problem: {}
+    }.with_indifferent_access # make sure to set this because the params object from rails is indifferential
+    @options = defaults.deep_merge(options)
+
+    @analysis_id = analysis_id
+    @analysis_job_id = analysis_job_id
+  end
+
+  # Perform is the main method that is run in the background.  At the moment if this method crashes
+  # it will be logged as a failed delayed_job and will fail after max_attempts.
+  def perform
+    @analysis = Analysis.find(@analysis_id)
+
+    # get the analysis and report that it is running
+    @analysis_job = Analysis::Core.initialize_analysis_job(@analysis, @analysis_job_id, @options)
+
+    # reload the object (which is required) because the subdocuments (jobs) may have changed
+    @analysis.reload
+
+    # At this point we should really setup the JSON that can be sent to the worker nodes with everything it needs
+    # This would allow us to easily replace the queuing system with rabbit or any other json based versions.
+
+    # get the master ip address
+    master_ip = ComputeNode.where(node_type: 'server').first.ip_address
+    Rails.logger.info("Master ip: #{master_ip}")
+    Rails.logger.info('Starting Local Runner')
+
+    # Quick preflight check that R, MongoDB, and Rails are working as expected. Checks to make sure
+    # that the run flag is true.
+    if @options[:data_points].empty?
+      Rails.logger.info 'No data points were passed into the options, therefore checking which data points to run'
+      @analysis.data_points.where(status: 'na', download_status: 'na').only(:status, :download_status, :uuid).each do |dp|
+        Rails.logger.info "Adding in #{dp.uuid}"
+        dp.status = 'queued'
+        dp.save!
+        @options[:data_points] << dp.uuid
+      end
+    end
+
+    # Initialize some variables that are in the rescue/ensure blocks
+    process = nil
+    begin
+
+      # Before kicking off the Analysis, make sure to setup the downloading of the files child process
+      process = Analysis::Core::BackgroundTasks.start_child_processes
+
+      `cd ? && bundle exec ruby local_init_final.rb arg arg arg`
+
+      @options[:data_points].each do |dp|
+        #TODO Fix which ruby to openstudio ruby that is shipped
+        string_to_exec = "cd /mnt/openstudio && bundle exec ruby /mnt/openstudio/simulate_data_point.rb -a #{@analysis.id} -u #{dp} -x #{@options[:run_data_point_filename]}"
+        Rails.logger.info "Attempting to exec string: \n #{string_to}"
+        system string_to_exec
+        Rails.logger.info "Ran datapoint #{dp}"
+      end
+
+    rescue => e
+      log_message = "#{__FILE__} failed with #{e.message}, #{e.backtrace.join("\n")}"
+      Rails.logger.error log_message
+      @analysis.status_message = log_message
+      @analysis.save!
+    ensure
+      # ensure that the cluster is stopped
+      cluster.stop if cluster
+
+      # Kill the downloading of data files process
+      Rails.logger.info('Ensure block of analysis cleaning up any remaining processes')
+      process.stop if process
+    end
+
+    Rails.logger.info 'Running finalize worker scripts'
+    unless cluster.finalize_workers(worker_ips, @analysis.id)
+      fail 'could not run finalize worker scripts'
+    end
+    # Do one last check if there are any data points that were not downloaded
+    begin
+      # in large analyses it appears that this is timing out or just not running to completion.
+      Rails.logger.info('Trying to download any remaining files from worker nodes')
+      @analysis.finalize_data_points
+    rescue => e
+      log_message = "#{__FILE__} failed with #{e.message}, #{e.backtrace.join("\n")}"
+      Rails.logger.error log_message
+      @analysis.status_message += log_message
+      @analysis.save!
+    ensure
+      # Only set this data if the analysis was NOT called from another analysis
+      unless @options[:skip_init]
+        @analysis_job.end_time = Time.now
+        @analysis_job.status = 'completed'
+        @analysis_job.save!
+        @analysis.reload
+      end
+      @analysis.save!
+
+      Rails.logger.info "Finished running analysis '#{self.class.name}'"
+    end
+  end
+
+  # Since this is a delayed job, if it crashes it will typically try multiple times.
+  # Fix this to 1 retry for now.
+  def max_attempts
+    1
+  end
+end
---
+++ worker-nodes/local_simulate_data_point.rb
@@ -0,0 +1,157 @@
+# Command line based interface to execute the Workflow manager.
+
+require 'bundler'
+begin
+  Bundler.setup
+rescue Bundler::BundlerError => e
+  $stderr.puts e.message
+  $stderr.puts 'Run `bundle install` to install missing gems'
+  exit e.status_code
+end
+
+require 'optparse'
+require 'fileutils'
+require 'logger'
+require 'openstudio-workflow'
+
+puts "Parsing Input: #{ARGV}"
+
+# parse arguments with optparse
+options = {}
+optparse = OptionParser.new do |opts|
+  opts.on('-a', '--analysis_id UUID', String, 'UUID of the analysis.') do |analysis_id|
+    options[:analysis_id] = analysis_id
+  end
+
+  options[:uuid] = 'NA'
+  opts.on('-u', '--uuid UUID', String, 'UUID of the data point to run with no braces.') do |s|
+    options[:uuid] = s
+  end
+
+  options[:run_workflow_method] = 'workflow'
+  opts.on('-x', '--execute-file NAME', String, 'Type of the workflow the run will execute') do |s|
+    options[:run_workflow_method] = s
+  end
+end
+optparse.parse!
+
+puts "Parsed Input: #{optparse}"
+
+puts 'Checking Arguments'
+unless options[:uuid]
+  # required argument is missing
+  puts optparse
+  exit
+end
+
+# Set the result of the project for R to know that this finished
+result = nil
+
+begin
+  directory = nil
+  fail 'Data Point is NA... skipping' if options[:uuid] == 'NA'
+  #TODO Fix paths
+  analysis_dir = "/mnt/openstudio/analysis_#{options[:analysis_id]}"
+  store_directory = "/mnt/openstudio/analysis_#{options[:analysis_id]}/data_point_#{options[:uuid]}"
+  FileUtils.mkdir_p(store_directory)
+
+  # use /run/shm on AWS (if possible)
+  directory = store_directory
+
+  # Logger for the simulate datapoint
+  logger = Logger.new("#{directory}/#{options[:uuid]}.log")
+
+  logger.info "Analysis Root Directory is #{analysis_dir}"
+  logger.info "Simulation Run Directory is #{directory}"
+  logger.info "Simulation Storage Directory is #{store_directory}"
+  logger.info "Run datapoint type/file is #{options[:run_workflow_method]}"
+
+  # TODO: program the various paths based on the run_type
+
+  # Set the default workflow options
+  #TODO Pass in path as argv
+  workflow_options = {
+    datapoint_id: options[:uuid],
+    analysis_root_path: analysis_dir,
+    adapter_options: {
+      mongoid_path: '/mnt/openstudio/rails-models'
+    }
+  }
+  if options[:run_workflow_method] == 'custom_xml' ||
+     options[:run_workflow_method] == 'run_openstudio_xml.rb'
+
+    # Set up the custom workflow states and transitions
+    transitions = OpenStudio::Workflow::Run.default_transition
+    transitions[1][:to] = :xml
+    transitions.insert(2, from: :xml, to: :openstudio)
+    states = OpenStudio::Workflow::Run.default_states
+    states.insert(2, state: :xml, options: { after_enter: :run_xml })
+
+    workflow_options = {
+      transitions: transitions,
+      states: states,
+      analysis_root_path: analysis_dir,
+      datapoint_id: options[:uuid],
+      xml_library_file: "#{analysis_dir}/lib/openstudio_xml/main.rb",
+      adapter_options: {
+        mongoid_path: '/mnt/openstudio/rails-models'
+      }
+    }
+  elsif options[:run_workflow_method] == 'pat_workflow' ||
+        options[:run_workflow_method] == 'run_openstudio.rb'
+    workflow_options = {
+      is_pat: true,
+      datapoint_id: options[:uuid],
+      analysis_root_path: analysis_dir,
+      adapter_options: {
+        mongoid_path: '/mnt/openstudio/rails-models'
+      }
+    }
+  end
+
+  logger.info 'Creating Workflow Manager instance'
+  k = OpenStudio::Workflow.load 'Mongo', directory, workflow_options
+  logger.info "Running workflow with #{options}"
+  k.run
+  logger.info "Final run state is #{k.final_state}"
+
+  # TODO: get the last results out --- result = result.split("\n").last if result
+
+  # copy the files that are needed over to the run directory
+  if options[:run_shm]
+    # only grab the zip/log files and put back in store_directory
+    zip_file = "#{directory}/data_point_#{options[:uuid]}.zip"
+    dest_zip_file = "#{store_directory}/data_point_#{options[:uuid]}.zip"
+    logger.info "Trying to move zip file from #{zip_file} to #{dest_zip_file}"
+    if File.exist?(zip_file)
+      FileUtils.rm_f(dest_zip_file) if File.exist?(dest_zip_file)
+      logger.info 'Moving zip file'
+      FileUtils.move(zip_file, dest_zip_file)
+    end
+
+    log_file = "#{directory}/#{options[:uuid]}.log"
+    dest_log_file = File.expand_path("#{store_directory}/../#{options[:uuid]}-run_os.log")
+    if File.exist?(log_file)
+      FileUtils.rm_f(dest_log_file) if File.exist?(dest_log_file)
+      FileUtils.move(log_file, dest_log_file)
+    end
+
+    logger.info "Removing directory from shm #{directory}"
+    FileUtils.rm_rf(directory) if Dir.exist?(directory)
+  end
+
+  # check if the simulation failed after moving the files back to the right place
+  if result == 'NA'
+    logger.info 'Simulation result was invalid'
+    fail 'Simulation result was invalid'
+  end
+rescue => e
+  log_message = "#{__FILE__} failed with #{e.message}, #{e.backtrace.join("\n")}"
+  puts log_message
+  logger.info log_message if logger
+ensure
+  logger.info "Finished #{__FILE__}" if logger
+  logger.close if logger
+  # always print the objective function result or NA
+  puts result
+end
