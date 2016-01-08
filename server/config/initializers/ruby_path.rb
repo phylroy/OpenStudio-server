a0cf6b699e283be616eae773d7773a0504079bc9
Brian Ball <Brian.Ball@nrel.gov>
2016-01-08 11:00:07 -0700

    trying to get openstudio-workflow to load

--- server/config/initializers/ruby_path.rb
+++ server/config/initializers/ruby_path.rb
@@ -1,4 +1,6 @@
 # Set the ruby path so that child processes and R know where to call it.
 
 #RUBY_BIN_DIR = '/opt/rbenv/shims'
-RUBY_BIN_DIR = `C:\Program Files\OpenStudio 1.10.0\ruby-install\ruby\bin`
+#RUBY_BIN_DIR = `C:\Program Files\OpenStudio 1.10.0\ruby-install\ruby\bin`
+RUBY_BIN_DIR = 'C:/Program Files/OpenStudio 1.10.0/ruby-install/ruby/bin'
+
