2e08fdac797dd189c22d57ab60786bb4d512fd6f
Brian Ball <Brian.Ball@nrel.gov>
2016-01-07 18:25:20 -0700

    trying to use OS ruby

--- server/config/initializers/ruby_path.rb
+++ server/config/initializers/ruby_path.rb
@@ -1,3 +1,4 @@
 # Set the ruby path so that child processes and R know where to call it.
 
-RUBY_BIN_DIR = '/opt/rbenv/shims'
+#RUBY_BIN_DIR = '/opt/rbenv/shims'
+RUBY_BIN_DIR = `C:\Program Files\OpenStudio 1.10.0\ruby-install\ruby\bin`
