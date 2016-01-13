22f9efb39aa0e7e3ecf489b1494d17fbf8a060d1
Brian Ball <Brian.Ball@nrel.gov>
2016-01-12 17:16:24 -0700

    First Successful Local Run. Have to modify workflow-gem to work. replace IO.popen with `@{energyplus_exe}` in \run_energyplus.rb

--- server/config/initializers/delayed_job_config.rb
+++ server/config/initializers/delayed_job_config.rb
@@ -1,3 +1,3 @@
 # Allow the jobs to run for up to 1 week.  If this is ever hit, then we have other problems.
 Delayed::Worker.max_run_time = 168.hours
-Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'dj.log'))
+Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_jobs.log'))
